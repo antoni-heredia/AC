1722fc23ab2463286f21208a12a97f7ee3442cad
Antonio Heredia <a.heredia.castillo@gmail.com>
2018-02-27 10:40:21 +0100

    Practica 0 hasta las graficas

---
+++ Practica0/Script1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Se asigna al trabajo el nombre SumaVectoresC_vlocales
+#PBS -N SumaVectoresC_vlocales
+#Se asigna al trabajo la cola ac
+#PBS -q ac  
+for ((N=65536;N<67108865;N=N*2))
+do
+	./SumaVectoresC $N
+done
